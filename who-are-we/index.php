2997d43a7cbef4d086a18c13bbce5c8aa3c9155e
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-12-06 11:57:00 -0500

    Add Who Are We page

---
+++ who-are-we/index.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Who Are We";
+$body_class = "who-are-we";
+require_once "../includes/header.php";
+?>
+    <section class="about">
+      <h2>Who Are We?</h2>
+
+      <p>Steering Committee:</p>
+
+      <ul>
+        <li><img src="/img/who-are-we/jeff-byrnes.jpg" alt="Headshot of Jeff Byrnes"> <a href="http://thejeffbyrnes.com/" title="Jeff Byrnes">Jeff Byrnes</a>, Ward 6</li>
+        <li><img src="/img/who-are-we/luke-edson.jpg" alt="Headshot of Luke Edson"> Luke Edson, Ward 6</li>
+        <li><img src="/img/who-are-we/hallah-elbeleidy.jpg" alt="Headshot of Hallah Elbeleidy"> Hallah Elbeleidy, Ward 2</li>
+        <li><img src="/img/who-are-we/laura-evans.jpg" alt="Headshot of Laura Evans"> Laura Evans, Ward 7</li>
+        <li><img src="/img/who-are-we/stephen-moore.jpg" alt="Headshot of Stephen Moore"> Stephen Moore, Ward 4</li>
+        <li><img src="/img/who-are-we/aaron-s-weber.jpg" alt="Headshot of Aaron S. Weber"> Aaron S. Weber, Ward 3</li>
+      </ul>
+
+      <hr>
+    </section>
+<?php
+require_once "../includes/footer.php";
