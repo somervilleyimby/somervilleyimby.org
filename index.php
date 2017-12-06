892e82e6604a363f51aecdb429c63a4107d35ff0
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-12-06 10:45:46 -0500

    Add Aaron Weber’s new copy for homepage

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ $body_class = "home";
 require_once "./includes/header.php";
 ?>
     <section class="about">
-      <p>Somerville YIMBY — Yes In My Back Yard — is a group of Somerville residents advocating for smart growth that benefits everyone. We’re a part of a nationwide movement to increase the amount of housing available to people in cities that have suffered from gentrification and expensive market rate housing.</p>
+      <p>Somerville YIMBY — Yes In My Back Yard — is a group of Somerville residents advocating for smart growth that benefits everyone. We’re a part of a nationwide movement to increase the amount of housing available to people in cities that have suffered from gentrification and expensive market rate housing. Like our friends A Better Cambridge, we support eco-friendly density and transit-centered growth to create vibrant, walkable, bikeable, livable neighborhoods with plenty of housing for all.</p>
 
-      <p>We believe that our cities and towns are great places, and we should welcome people who want to be here by building them places to live. We believe there is a desirable, achievable balance between subsidized, moderately-priced market rate, and high-end development that can satisfy residents and businesses in a mutually beneficial way.</p>
+      <p>We believe that our cities and towns are great places, and we should welcome people who want to be here by creating plenty of housing for new neighbors of all income levels.</p>
 
       <hr>
     </section>
