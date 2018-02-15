b3755ae8674dad2859795db9d92fbbd124a2bc8f
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-02-15 11:05:22 -0500

    Fix responsiveness for small screens

--- calendar/index.php
+++ calendar/index.php
@@ -8,8 +8,12 @@ require_once "../includes/header.php";
 
       <p>This calendar is a collection of city & neighborhood meetings, and other events, relevant to advocating for smart growth in Somerville & the Boston metro region. It is not exhaustive, so if you have an event you’d like added, please reach out to <a href="mailto:steering+addtocalendar@somervilleyimby.org">steering@somervilleyimby.org</a></p>
 
-      <div class="google-calendar">
-        <iframe src="https://calendar.google.com/calendar/embed?src=somervilleyimby.org_8duipmcoo11uvtvchrj1gqp0n4%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <div class="google-calendar big-container">
+        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&showNav=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&height=700&src=somervilleyimby.org_8duipmcoo11uvtvchrj1gqp0n4%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      </div>
+
+      <div class="google-calendar small-container">
+        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&showNav=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&height=700&mode=AGENDA&src=somervilleyimby.org_8duipmcoo11uvtvchrj1gqp0n4%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
 
       <hr>
