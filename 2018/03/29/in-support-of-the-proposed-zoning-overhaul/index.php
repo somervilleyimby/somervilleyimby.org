88228041328aff18e95f2640763bc2a44fc732e7
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-05-24 17:12:06 -0400

    Add previously sent letter re: zoning reform
    Should have added this when it was sent originally.

---
+++ 2018/03/29/in-support-of-the-proposed-zoning-overhaul/index.php
@@ -0,0 +1,36 @@
+<?php
+$title = "In support of the Proposed Zoning Overhaul";
+$body_class = "";
+require_once "../../../../includes/header.php";
+?>
+    <article class="main letter">
+      <p>The following is a letter we emailed to the Somerville Board of Alderman on <time datetime="2018-03-29T08:43-04:00">May 24, 2018</time>, regarding the reform of the City’s zoning regulations.</p>
+
+      <blockquote>
+        <p>To the City of Somerville and the Board of Aldermen:</p>
+
+        <p>Somerville YIMBY is writing to support the new zoning regulations. The planning staff has done an admirable job, both in writing the regulations and in communicating with the community throughout the process. The new rules are a step in the right direction, and a beneficial simplification and increase in usability for the citizens and businesses of the city.</p>
+
+        <p>We particularly like the unbundling of parking from housing and the sensible reductions in mandatory parking creation near transit. This prioritization of space for people over space for cars should help to moderate costs in these desirable neighborhoods. In many cases, we think that the parking minimums could be lowered still further or even eliminated, especially near transit hubs.</p>
+
+        <p>We have several additional changes to recommend.</p>
+
+        <p>First, the Neighborhood Residence (NR) district should allow three-family homes, and the “house” building designation should be expanded to include three-unit buildings. This is notable especially in the case of our iconic and beloved triple-deckers, but three-unit residences are broadly supported, practical, and entirely reasonable additions to our neighborhoods.</p>
+
+        <p>Second, by-right construction should be truly by right. In the proposed code, adding units to an existing building requires a special permit, even when one would not be required to build something similar from scratch. The special permit process should be reserved for projects which are truly unusual or unanticipated.</p>
+
+        <p>Third, the city should follow the lead of Cambridge in making it easier to construct accessory dwelling units, or “in-law apartments.” These small infill residences can provide moderately-priced apartments, help keep extended families together, and make it possible for seniors to remain in their neighborhoods. We respect that it may not be possible to address this issue immediately, but it should be next on the agenda.</p>
+
+        <p>Sincerely,</p>
+
+        <p><strong>Somerville YIMBY Steering Committee</strong></p>
+        <p>Aaron Weber</p>
+        <p>Jeff Byrnes</p>
+        <p>Laura Evans</p>
+        <p>Hallah Elbeleidy</p>
+        <p>Stephen Moore</p>
+        <p>Luke Edson</p>
+      </blockquote>
+      </blockquote>
+    </article>
+<?php require_once "../../../../includes/footer.php";
