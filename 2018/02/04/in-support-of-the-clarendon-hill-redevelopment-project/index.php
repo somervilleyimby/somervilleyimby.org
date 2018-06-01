c2c2b15b4be0794b332c08e96f37607321176551
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-06-01 12:11:25 -0400

    Add 2018-02-04 letter to BoA re: Clarendon Hill
    An oversite that this was not posted when it was sent to the Board

---
+++ 2018/02/04/in-support-of-the-clarendon-hill-redevelopment-project/index.php
@@ -0,0 +1,30 @@
+<?php
+$title = "In support of the Clarendon Hill Redevelopment Project";
+$body_class = "";
+require_once "../../../../includes/header.php";
+?>
+    <article class="main letter">
+      <p>The following is a letter we emailed to the Somerville Board of Alderman on <time datetime="2018-02-04T10:53-05:00">February 04, 2018</time>, regarding the reform of the effort to rebuild <a href="http://clarendonhill.org">Clarendon Hill</a>.</p>
+
+      <blockquote>
+        <p>Esteemed members of the Board of Aldermen:
+
+        <p>Somerville YIMBY is writing to express our support for the Clarendon Hill Redevelopment Project. We believe that there is an urgent need for the project to move forward in the development process to ensure that currently committed state funding is not lost.</p>
+
+        <p>The Clarendon Hill Redevelopment Project will be a major benefit to the City of Somerville. Somerville and the Greater Boston Area are in the midst of a housing affordability crisis. This project will help ease this crisis by expanding the stock of both affordable and market-rate housing.</p>
+
+        <p>Further, it is clear that the existing Clarendon Hill development is in urgent need of renovation. The proposed redevelopment will be subsidized by the market-rate developer and State and Federal funding sources, easing the burden of this inevitable expense on the City. The plans provide for all of the 216 families residing there to be relocated during construction, and ensures they can all return once it is complete.</p>
+
+        <p>Time is of the essence. This project has already undergone significant public discussion (eight meetings and counting), and been adjusted to respond to the community’s concerns. Further delay will endanger State funding for the project, putting the project in jeopardy. There are, without a doubt, questions that the community still needs answered.</p>
+
+        <p>However, further details of the project will not be known until a formal ZBA proposal has been completed. There will still be time for community members to have their questions answered and their concerns heard, but it is time for the project to progress to the next phase.</p>
+
+        <p>We want to thank the Mayor and Alderman Katjana Ballantyne for their leadership on this project.</p>
+
+        <p>We hope the community will reach out to their city representatives to express support for the project.</p>
+
+        <p>Sincerely,</p>
+        <p>Somerville YIMBY</p>
+      </blockquote>
+    </article>
+<?php require_once "../../../../includes/footer.php";
