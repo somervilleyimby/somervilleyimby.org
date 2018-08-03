16db50738c149fc4a32a5c4a8ea6886af0e6fb04
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-08-03 15:25:42 -0400

    Loosen Capistrano lock
    Allows all non-breaking changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.10.0'
+lock '~> 3.10'
 
 set :application, 'somervilleyimby.org'
 set :repo_url, 'git@github.com:jeffbyrnes/somervilleyimby.org.git'
