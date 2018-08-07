9da3c82aaa35ccb302369fcd6d1239e06f11fb48
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-08-07 17:04:04 -0400

    Add St James Place letter to CambMA planning board

---
+++ 2018/08/07/st-james-place-good-cause/index.php
@@ -0,0 +1,30 @@
+<?php
+$title = "Re: St. James Project; Special Permit";
+$body_class = "";
+require_once "../../../../includes/header.php";
+?>
+    <article class="main letter">
+      <p>The following is a letter we emailed to the Cambridge Planning Board on <time datetime="2018-07-30T15:36-04:00">July 30, 2018</time>, regarding a special permit issued to the St. James Place redevelopment. We sent this letter because housing is a regional issue, and this project, especially, is in close proximity to Somerville.</p>
+
+      <blockquote>
+        <p>Dear Chair, Vice Chair and Members of the Planning Board:</p>
+
+        <p>Somerville YIMBY advocates for housing affordability, a regional issue, so we are reaching out to encourage you to allow this excellent project to finally move forward. This project is in close proximity to Somerville, and will have an impact on housing in both cities.</p>
+
+        <p>We urge you to find that the project has not started for “good cause” under the Cambridge zoning code.</p>
+
+        <p>While the evidence supporting “good cause” is clear, we want to make two additional points to counter communications you may receive from some.</p>
+
+        <p>This is an excellent project that will benefit the Porter Square and Davis Square neighborhoods. It helps the historic St. James’s Church rebuild its parish house, replaces a now-defunct carwash, and rebuilds a garden, open to the public. It adds 46 new homes, 5 of which will be set aside for low income residents.</p>
+
+        <p>The project has been delayed by a steady stream of lawsuits with the purpose of killing it through delay. For many years, the neighborhood has tolerated closed and blighted buildings (the car wash and the parish house) while the lawsuits, one after the other, have been resolved in favor of the developer, Oaktree.</p>
+
+        <p>Again, we ask you to find that this project has not started for “good cause”, and allow it to proceed.</p>
+
+        <p>Thank you for your time and consideration.</p>
+
+        <p>Sincerely,<br>
+        Somerville YIMBY</p>
+      </blockquote>
+    </article>
+<?php require_once "../../../../includes/footer.php";
