e44d437f6d5c8eb5498ded33407300b442af90e6
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-08-15 15:21:46 -0400

    Add Union Sq D2 MEPA waiver letter

---
+++ 2018/08/15/union-square-d2-mepa/index.php
@@ -0,0 +1,43 @@
+<?php
+$title = "Re: Union Square - EEA#15889";
+$body_class = "";
+require_once "../../../../includes/header.php";
+?>
+    <article class="main letter">
+      <p>The following is a letter we emailed to MEPA on <time datetime="2018-08-15T15:18-04:00">August 15, 2018</time>, regarding the “Phase 1 waiver” (it’s not a waiver) for the D2 block of US2’s Union Square redevelopment.</p>
+
+      <blockquote>
+        <p>Via e-mail to: alexander.strysky@state.ma.us<br>
+        Re: Union Square - EEA#15889</p>
+
+        <p>Matthew Beaton, Secretary of Energy and Environmental Affairs (EEA)<br>
+        100 Cambridge Street, Suite 900<br>
+        Boston MA 02114</p>
+
+        <p>Dear Secretary Beaton:</p>
+
+        <p>We write in support of the Union Square “D2” project receiving accelerated review and moving toward approval.</p>
+
+        <p>Somerville YIMBY — Yes In My Back Yard — is a group of Somerville residents advocating for smart growth that benefits everyone. We’re a part of a nationwide movement to increase the amount of housing available to people in cities that have suffered from gentrification and expensive market rate housing. Like our friends <a href="http://abettercambridge.org">A Better Cambridge</a>, we support eco-friendly density and transit-centered growth to create vibrant, walkable, bikeable, livable neighborhoods with plenty of housing for all. We believe that our cities and towns are great places, and we should welcome people who want to be here by creating plenty of housing for new neighbors of all income levels.</p>
+
+        <p>We appreciate the work that US2 has done to engage in an extensive community planning process to shape the proposed project, particularly the first phase, and would like to see this first phase move forward in parallel with the new Union Square Station. </p>
+
+        <p>The project clearly meets the “severability” criteria for a Phase 1 Waiver, since it is a separate block from other phases of the overall Union Square revitalization project.</p>
+
+        <p>In addition, it provides clear environmental benefits:</p>
+
+        <ul>
+          <li>Construction begins with site remediation, alleviating concerns about ground contaminants.</li>
+          <li>The project is tightly linked with upgraded sewer and storm systems, which will reduce water contamination and improve climate resiliency</li>
+          <li>The project is critical to the completion of the Union Square MBTA station, which will enable Somerville to reduce car travel and increase sustainable mobility.</li>
+          <li>Construction includes separated bike lanes and bike parking, again enhancing sustainable mobility for the community.</li>
+          <li>The buildings on the site will be LEED Gold certified, with all the attendant environmental benefits that certification implies.</li>
+          <li>Increased commercial and residential density near transit hubs like Union Square reduces auto trips and carbon emissions and is broadly in keeping with the sustainability goals of the city and the state.</li>
+          <li>This long-awaited development is an important part of both environmental and community goals for the city of Somerville. Please approve its MEPA Phase 1 Waiver and help it reach completion on time.</li>
+        </ul>
+
+        <p>Sincerely,<br>
+        <a href="https://somervilleyimby.org/who-are-we/">Somerville YIMBY Steering Committee</a></p>
+      </blockquote>
+    </article>
+<?php require_once "../../../../includes/footer.php";
