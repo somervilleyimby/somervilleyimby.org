ef164d0ecf9e1518258ea5a41c0109aca287e61e
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-10-30 15:31:54 -0400

    Add 2018-10-30 zoning overhaul letter

---
+++ 2018/10/30/regarding-the-proposed-zoning-overhaul/index.php
@@ -0,0 +1,47 @@
+<?php
+$title = "Regarding the Proposed Zoning Overhaul";
+$body_class = "";
+require_once "../../../../includes/header.php";
+?>
+    <article class="main letter">
+      <p>The following is a letter we emailed to the Somerville Board of Alderman on <time datetime="2018-10-30T15:07-04:00">Oct 30, 2018</time>, regarding the reform of the City’s zoning regulations.</p>
+
+      <blockquote>
+        <p>To the Board of Aldermen:</p>
+
+        <p>The current version of the Somerville Zoning Ordinance has a great deal to recommend it, and is a major improvement to the current ordinance in terms of its clarity, usefulness, and practicality. We are optimistic that it will be ready to be implemented before the end of the year.</p>
+
+        <p>Recognizing that this may be one of the last opportunities to make changes before the initial implementation, we would like to make the following suggestions.</p>
+
+        <p><strong>Gross Floor Area to Dwelling Unit Ratios:</strong> We are encouraged by the increase in permitted density under the proposed code. However, it still sets a maximum number of units in any building based on its total floor area, a calculation that is not based on safety but on limiting density. Limitations of this sort will reduce the amount of housing that can be created, increase prices, and ensure that new construction continues to be tilted more heavily toward high-priced “luxury” development. Small studio and 1-bedroom apartments and condos are a great match with today’s smaller households, and we should not artificially restrict the availability of these modestly-priced, modestly-sized homes. These ratios should be eliminated or reduced further.</p>
+
+        <p><strong>Triple-Deckers:</strong> We are encouraged by the addition of triple-decker and carriage house types to the NR district. However, we think it would be entirely reasonable to permit the construction of our beloved, iconic triple-deckers by-right in all locations in the NR district.</p>
+
+        <p><strong>Accessory Dwelling Units:</strong> The rules surrounding ADUs are unnecessarily complex and would be almost certain to stymie the creation of these necessary middle-income homes.</p>
+        <ul class="default">
+          <li>The special permit rule: Moving the special permit requirement from the construction phase to the occupancy-permit phase increases, rather than decreases, the uncertainty in the process.</li>
+          <li>The owner-occupant landlord rule: Advocates for the owner-occupancy rule claim that it promotes individual home ownership, but it actually tilts the benefits of ADU construction to people with enough wealth to own multifamily buildings. The rule also creates a complex enforcement and monitoring burden for the city, one which could become very expensive to administer.</li>
+          <li>Noise and nuisance concerns: Proponents of the owner-occupant landlord rule also cite noise and nuisance problems they claim are allayed by onsite landlord supervision. However, an onsite landlord is no guarantee of tenant behavior. If the city wishes to address noise and nuisance issues, it should use its existing powers to address those issues.</li>
+          <li>Banning subdivision and sale: If the city has a stated goal of promoting homeownership and owner-occupancy, why would we prohibit making an ADU a home that someone could own?</li>
+        </ul>
+
+        <p><strong>Number of Related Residents:</strong> The rule is counterproductive in numerous ways. It ignores the complexity of contemporary household formation; it inserts the municipal government into personal relationships in inappropriate ways; it blocks the creation of moderately priced alternative housing; and it creates opportunities for unscrupulous landlords to exploit tenants who fear being evicted if they report safety violations to the city.</p>
+
+        <p>Advocates for this rule say it prevents noise and preserves housing for traditional families with children. This is nonsense. If the city wishes to reduce noise, it should enforce the existing noise ordinance. If it wishes to make the city child-friendly, it should offer incentives and benefits directly to families with children. The unrelated-residents rule achieves none of the city’s actual goals, while increasing prices and harming people who want to share a home.</p>
+
+        <p><strong>Beacon St., Washington St., and Highland Ave Corridors:</strong> Significant portions of the Washington Street, Beacon Street, and Highland Avenue corridors need greater height and density. We believe that well-planned upzoning will provide more homes in these areas without threatening their personality.</p>
+
+        <p>In particular, we believe that the focus on preserving existing 1-story commercial buildings is misguided. We agree with the city’s goal of preserving grocery stores and other valuable neighborhood retail. However, as the Broadway Star Market site shows, a one-story building is not a guarantee that a grocery store will continue to operate. Moreover, there is no reason a grocery store cannot operate in a taller building, as the b.fresh in Davis Square and numerous Star Market locations throughout the Boston area demonstrate. If the city wishes to preserve grocery stores, then it should do so directly, not dictate that the shape of existing grocery store buildings remain unchanged.</p>
+
+        <p><strong>Issues of Equity:</strong> We should acknowledge that the much of the “conserve” elements of SomerVision, which this zoning ordinance takes as its guidance, are in West Somerville, which has a larger proportion of white residents, while East Somerville appears to receive more of the “transform” and “enhance” elements. Especially glaring is that Davis Square, with the lone Red Line station in our entire city, is not upzoned for considerably greater future development. The city absolutely needs to increase the permitted height and density for locations like Davis Square if it is to avoid disproportionate development impact to minority neighborhoods.</p>
+
+        <p><strong>Transit Oriented Development:</strong> If the United States is to do its part in mitigating the oncoming disaster that is global climate change, cities need to get people out of cars and onto public transit. That can only happen if people live near transit. Somerville’s role is clear: Permit more homes and businesses near Green Line and Red Line stations.</p>
+
+        <p>Thank you for your work on this important project. We hope that our contributions are helpful and informative, and we look forward to collaborating with the city on these issues in the future.</p>
+
+        <p>Sincerely,</p>
+
+        <p><strong>Somerville YIMBY Steering Committee</strong></p>
+      </blockquote>
+    </article>
+<?php require_once "../../../../includes/footer.php";
