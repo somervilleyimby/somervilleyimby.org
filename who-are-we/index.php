5860c190c6a01849b3ad3b3c0e355b2b2656ce69
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-12-19 11:17:30 -0500

    Remove Hallah Elbeleidy from Who Are We page
    She stepped down from the Steering Committee on Dec 17, 2018

--- who-are-we/index.php
+++ who-are-we/index.php
@@ -11,7 +11,6 @@ require_once "../includes/header.php";
       <ul>
         <li><img src="/img/who-are-we/jeff-byrnes.jpg" alt="Headshot of Jeff Byrnes"> <a href="http://thejeffbyrnes.com/" title="Jeff Byrnes">Jeff Byrnes</a>, Ward 6</li>
         <li><img src="/img/who-are-we/luke-edson.jpg" alt="Headshot of Luke Edson"> Luke Edson, Ward 6</li>
-        <li><img src="/img/who-are-we/hallah-elbeleidy.jpg" alt="Headshot of Hallah Elbeleidy"> Hallah Elbeleidy, Ward 2</li>
         <li><img src="/img/who-are-we/laura-evans.jpg" alt="Headshot of Laura Evans"> Laura Evans, Ward 7</li>
         <li><img src="/img/who-are-we/stephen-moore.jpg" alt="Headshot of Stephen Moore"> Stephen Moore, Ward 4</li>
         <li><img src="/img/who-are-we/aaron-s-weber.jpg" alt="Headshot of Aaron S. Weber"> Aaron S. Weber, Ward 3</li>
