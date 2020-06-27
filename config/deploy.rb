3bce0cb9a2ce2926df6c55a13a48377e19a6e5df
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-06-27 12:42:56 -0400

    Update deploy config for DO Droplet

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,8 @@ lock '~> 3.10'
 
 set :application, 'somervilleyimby.org'
 set :repo_url, 'git@github.com:jeffbyrnes/somervilleyimby.org.git'
+set :branch, 'main'
 
-server 'aws-jb', user: 'deploy', roles: %w(web app)
+server 'do-jb', user: 'deploy', roles: %w[web app]
 
 before 'deploy:finished', 'sass:default'
