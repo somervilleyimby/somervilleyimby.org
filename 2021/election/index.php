2087e23aaa7bb98a91848626407d22d47a530290
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-05-25 12:53:45 -0400

    Fix path to include header for 2021 election page

--- 2021/election/index.php
+++ 2021/election/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = "2021 Somerville Election Candidate Survey";
 $body_class = "";
-require_once "../../../../includes/header.php";
+require_once "../../includes/header.php";
 ?>
     <article class="main">
       <p>For the 2021 elections for City Council and Mayor in Somerville, we’ve put together a survey for all of the candidates, to better understand their positions on housing, and how it relates to sustainability and affordability.</p>
