367f2e76da54f12338fedbaa7bd076bcdfd976d7
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-05-25 10:18:20 -0400

    Make RuboCop happy (again)

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ set :application, 'somervilleyimby.org'
 set :repo_url, 'git@github.com:jeffbyrnes/somervilleyimby.org.git'
 set :branch, 'main'
 
-server 'do-jb', user: 'deploy', roles: %w[web app]
+server 'do-jb', user: 'deploy', roles: %w(web app)
 
 before 'deploy:finished', 'sass:default'
