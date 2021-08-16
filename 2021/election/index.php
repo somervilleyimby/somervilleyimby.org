cff6f5faea28262aade4407ea6c9c3ca6ab9cec8
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-08-16 11:48:00 -0400

    Add 2021 election survey links
    Quick & dirty, link to PDFs of the responses

--- 2021/election/index.php
+++ 2021/election/index.php
@@ -1,11 +1,107 @@
 <?php
 $title = "2021 Somerville Election Candidate Survey";
-$body_class = "";
+$body_class = "election";
 require_once "../../includes/header.php";
 ?>
     <article class="main">
+      <h1>2021 Somerville YIMBY Election Survey</h1>
+
       <p>For the 2021 elections for City Council and Mayor in Somerville, we’ve put together a survey for all of the candidates, to better understand their positions on housing, and how it relates to sustainability and affordability.</p>
 
-      <p>We’ll be posting the responses we’ve received soon!</p>
+      <p>Each candidate’s survey is linked from their name as a PDF.</p>
+
+      <h2>Candidates:</h2>
+
+      <p><em>Incumbents are indicated with an <span class="incumbent">*</span></em></p>
+
+      <ul class="default candidates">
+        <li>
+          <h2>Mayoral Election</h2>
+
+          <ul class="default">
+            <li><a href="https://drive.google.com/file/d/1-KcQV-E3IdwVQegkFd9OxqLWB_efZbYS/view?usp=sharing">Katjana Ballantyne</a</li>
+            <li><a href="https://drive.google.com/file/d/1-m28g1LdSTKSDoyiqshqEVA7zrPkhHfu/view?usp=sharing">Mary Cassesso</a></li>
+            <li>Wilfred “Will” Mbah (no answers provided)</li>
+            <li>William “Billy” Tauro (no answers provided)</li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>At-large</h2>
+
+          <ul class="default">
+            <li><a href="https://drive.google.com/file/d/1-efNkIbd62zl6hvzmKVRrqHe7bRjtZ-c/view?usp=sharing">Willie Burnley Jr</a></li>
+            <li><a href="https://drive.google.com/file/d/1-dA-IAL4vYjGQX2WyClC9a02oPTH_C0m/view?usp=sharing">Virginia Hussey</a></li>
+            <li><a href="https://drive.google.com/file/d/1-W5LvOF0nyTspp5-toR0v1sPGJoCAzSH/view?usp=sharing">Charlotte Kelly</a></li>
+            <li><a href="https://drive.google.com/file/d/1-SFMjD-4ou254AQEPw_RzhgwX-JG0QHc/view?usp=sharing">Justin Klekota</a></li>
+            <li><a href="https://drive.google.com/file/d/1-cv0306ME1mZIIXiK9f9n7yNjS2dGpIb/view?usp=sharing">Tracey Leah Pratt</a></li>
+            <li>Eve Seitchik (no answers provided)</a></li>
+            <li><a href="https://drive.google.com/file/d/1-WZq9gTbzRZU68LU9r7pNDrBg5eE5T92/view?usp=sharing">Kristen Strezo <span class="incumbent">*</span></a></li>
+            <li><a href="https://drive.google.com/file/d/1-gAVQcnNBN9ueWj-XiXTOwzGzDJIifly/view?usp=sharing">Jake Wilson</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 1</h2>
+
+          <ul class="default">
+            <li>Matt McLaughlin <span class="incumbent">*</span> (no answers provided)</li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 2</h2>
+
+          <ul class="default">
+            <li><a href="https://drive.google.com/file/d/1-bhoHRNfIj-VNLiKcCzKfqM9Jzplg7al/view?usp=sharing">Stephenson Aman</a></li>
+            <li><a href="https://drive.google.com/file/d/1-LhDukn3OYTsUAttv70zU9Fq1-8l_YEw/view?usp=sharing">JT Scott <span class="incumbent">*</span></a></li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 3</h2>
+
+          <ul class="default">
+            <li><a href="https://drive.google.com/file/d/1-LEMnRxNOvwfTnfPUHJ8ebbd8jVfm8qB/view?usp=sharing">Ben Ewen-Campen <span class="incumbent">*</span></a></li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 4</h2>
+
+          <ul class="default">
+            <li>Jesse Clingan <span class="incumbent">*</span> (no answers provided)</li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 5</h2>
+
+          <ul class="default">
+            <li><a href="https://drive.google.com/file/d/1-k89jnkD2BZrgpU4q7nY746D488f_8pj/view?usp=sharing">Tessa Bridge</a></li>
+            <li><a href="https://drive.google.com/file/d/1-hpM-dVRccjZyayD42BvMvWBqbQ7MqZj/view?usp=sharing">Todd Easton</a></li>
+            <li><a href="https://drive.google.com/file/d/1-aInfhVwdcptVOeoQJEw1yLsKr7aZG4n/view?usp=sharing">Beatriz Gómez-Mouakad</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 6</h2>
+
+          <ul class="default">
+            <li>Lance Davis <span class="incumbent">*</span> (no answers provided)</li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Ward 7</h2>
+
+          <ul class="default">
+            <li><a href="https://drive.google.com/file/d/1-SrCRrUzvnhGFmVaFfYHjGtGFMUvMWJs/view?usp=sharing">Alex Anderson</a></li>
+            <li><a href="https://drive.google.com/file/d/1-EGBqYKGQTCAB8j6K6ys7X0LgwV5eXJg/view?usp=sharing">Maria Koutsoubaris</a></li>
+            <li><a href="https://drive.google.com/file/d/1-gZW8SgYbfnKQjlNOSjPkKhkh51geAl4/view?usp=sharing">Becca Miller</a></li>
+            <li><a href="https://drive.google.com/file/d/1-ZkO5Bqbl9cj5g6dafnGbc4NIan4CGGw/view?usp=sharing">Judy Pineda Neufeld</a></li>
+          </ul>
+        </li>
+      </ul>
     </article>
 <?php require_once "../../includes/footer.php";
