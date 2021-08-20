f41b8be8f3440b754168e31b42925b40b04e9353
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-08-20 13:49:21 -0400

    Add endorsements & notes to 2021 election page

--- 2021/election/index.php
+++ 2021/election/index.php
@@ -3,105 +3,233 @@ $title = "2021 Somerville Election Candidate Survey";
 $body_class = "election";
 require_once "../../includes/header.php";
 ?>
-    <article class="main">
-      <h1>2021 Somerville YIMBY Election Survey</h1>
+<article class="main">
+  <h1>2021 Somerville YIMBY Voter Guide and Endorsements</h1>
 
-      <p>For the 2021 elections for City Council and Mayor in Somerville, we’ve put together a survey for all of the candidates, to better understand their positions on housing, and how it relates to sustainability and affordability.</p>
+  <p>Somerville YIMBY circulated a short survey to candidates for mayor and city council this summer. While all candidate surveys are limited in scope and necessarily simplify complex issues, our survey definitely erred on the side of over-simplification, especially in trying to squeeze Yes/No answers out of complex questions. We received excellent feedback from our respondents, and we acknowledge that it has a number of shortcomings. Please use this as a supplement to your own decisions.</p>
 
-      <p>Each candidate’s survey is linked from their name as a PDF.</p>
+  <p>We have also provided our endorsements and an explanation of them below. Our baseline goal is to find candidates who, regardless of their ideological background, intend to:</p>
 
-      <h2>Candidates:</h2>
+  <ul class="default">
+    <li>Increase the total amount of housing in the city</li>
+    <li>Move the city away from auto-centric development and excessive parking</li>
+    <li>Enhance tenant protections</li>
+  </ul>
 
-      <p><em>Incumbents are indicated with an <span class="incumbent">*</span></em></p>
+  <p>First, we present each candidate’s survey responses, linked from their name as a PDF. Following this list we elaborate on our choices for endorsement, as well as other candidates we feel would make good elected officials.</p>
 
-      <ul class="default candidates">
-        <li>
-          <h2>Mayoral Election</h2>
+  <p><em>Incumbents are indicated with an <span class="incumbent">*</span></em></p>
 
-          <ul class="default">
-            <li><a href="https://drive.google.com/file/d/1-KcQV-E3IdwVQegkFd9OxqLWB_efZbYS/view?usp=sharing">Katjana Ballantyne</a</li>
-            <li><a href="https://drive.google.com/file/d/1-m28g1LdSTKSDoyiqshqEVA7zrPkhHfu/view?usp=sharing">Mary Cassesso</a></li>
-            <li>Wilfred “Will” Mbah (no answers provided)</li>
-            <li>William “Billy” Tauro (no answers provided)</li>
-          </ul>
-        </li>
+  <p><em>Endorsements are in <strong>bold</strong>.</em></p>
 
-        <li>
-          <h2>At-large</h2>
+  <section>
+    <h2>Candidates:</h2>
 
-          <ul class="default">
-            <li><a href="https://drive.google.com/file/d/1-efNkIbd62zl6hvzmKVRrqHe7bRjtZ-c/view?usp=sharing">Willie Burnley Jr</a></li>
-            <li><a href="https://drive.google.com/file/d/1-dA-IAL4vYjGQX2WyClC9a02oPTH_C0m/view?usp=sharing">Virginia Hussey</a></li>
-            <li><a href="https://drive.google.com/file/d/1-W5LvOF0nyTspp5-toR0v1sPGJoCAzSH/view?usp=sharing">Charlotte Kelly</a></li>
-            <li><a href="https://drive.google.com/file/d/1-SFMjD-4ou254AQEPw_RzhgwX-JG0QHc/view?usp=sharing">Justin Klekota</a></li>
-            <li><a href="https://drive.google.com/file/d/1-cv0306ME1mZIIXiK9f9n7yNjS2dGpIb/view?usp=sharing">Tracey Leah Pratt</a></li>
-            <li>Eve Seitchik (no answers provided)</a></li>
-            <li><a href="https://drive.google.com/file/d/1-WZq9gTbzRZU68LU9r7pNDrBg5eE5T92/view?usp=sharing">Kristen Strezo <span class="incumbent">*</span></a></li>
-            <li><a href="https://drive.google.com/file/d/1-gAVQcnNBN9ueWj-XiXTOwzGzDJIifly/view?usp=sharing">Jake Wilson</a></li>
-          </ul>
-        </li>
+    <ul class="default candidates">
+      <li>
+        <h2>Mayoral Election</h2>
 
-        <li>
-          <h2>Ward 1</h2>
+        <ul class="default">
+          <li><strong><a href="https://drive.google.com/file/d/1-KcQV-E3IdwVQegkFd9OxqLWB_efZbYS/view?usp=sharing">Katjana Ballantyne</a></strong></li>
+          <li><strong><a href="https://drive.google.com/file/d/1-m28g1LdSTKSDoyiqshqEVA7zrPkhHfu/view?usp=sharing">Mary Cassesso</a></strong></li>
+          <li>Wilfred “Will” Mbah (no answers provided)</li>
+          <li>William “Billy” Tauro (no answers provided)</li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li>Matt McLaughlin <span class="incumbent">*</span> (no answers provided)</li>
-          </ul>
-        </li>
+      <li>
+        <h2>At-large</h2>
 
-        <li>
-          <h2>Ward 2</h2>
+        <ul class="default">
+          <li><strong><a href="https://drive.google.com/file/d/1-efNkIbd62zl6hvzmKVRrqHe7bRjtZ-c/view?usp=sharing">Willie Burnley Jr</a></strong></li>
+          <li><strong><a href="https://drive.google.com/file/d/1-dA-IAL4vYjGQX2WyClC9a02oPTH_C0m/view?usp=sharing">Virginia Hussey</a></strong></li>
+          <li><a href="https://drive.google.com/file/d/1-W5LvOF0nyTspp5-toR0v1sPGJoCAzSH/view?usp=sharing">Charlotte Kelly</a></li>
+          <li><a href="https://drive.google.com/file/d/1-SFMjD-4ou254AQEPw_RzhgwX-JG0QHc/view?usp=sharing">Justin Klekota</a></li>
+          <li><a href="https://drive.google.com/file/d/1-cv0306ME1mZIIXiK9f9n7yNjS2dGpIb/view?usp=sharing">Tracey Leah Pratt</a></li>
+          <li>Eve Seitchik (no answers provided)</a></li>
+          <li><a href="https://drive.google.com/file/d/1-WZq9gTbzRZU68LU9r7pNDrBg5eE5T92/view?usp=sharing">Kristen Strezo <span class="incumbent">*</span></a></li>
+          <li><strong><a href="https://drive.google.com/file/d/1-gAVQcnNBN9ueWj-XiXTOwzGzDJIifly/view?usp=sharing">Jake Wilson</a></strong></li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li><a href="https://drive.google.com/file/d/1-bhoHRNfIj-VNLiKcCzKfqM9Jzplg7al/view?usp=sharing">Stephenson Aman</a></li>
-            <li><a href="https://drive.google.com/file/d/1-LhDukn3OYTsUAttv70zU9Fq1-8l_YEw/view?usp=sharing">JT Scott <span class="incumbent">*</span></a></li>
-          </ul>
-        </li>
+      <li>
+        <h2>Ward 1</h2>
 
-        <li>
-          <h2>Ward 3</h2>
+        <ul class="default">
+          <li>Matt McLaughlin <span class="incumbent">*</span> (no answers provided)</li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li><a href="https://drive.google.com/file/d/1-LEMnRxNOvwfTnfPUHJ8ebbd8jVfm8qB/view?usp=sharing">Ben Ewen-Campen <span class="incumbent">*</span></a></li>
-          </ul>
-        </li>
+      <li>
+        <h2>Ward 2</h2>
 
-        <li>
-          <h2>Ward 4</h2>
+        <ul class="default">
+          <li><a href="https://drive.google.com/file/d/1-bhoHRNfIj-VNLiKcCzKfqM9Jzplg7al/view?usp=sharing">Stephenson Aman</a></li>
+          <li><a href="https://drive.google.com/file/d/1-LhDukn3OYTsUAttv70zU9Fq1-8l_YEw/view?usp=sharing">JT Scott <span class="incumbent">*</span></a></li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li>Jesse Clingan <span class="incumbent">*</span> (no answers provided)</li>
-          </ul>
-        </li>
+      <li>
+        <h2>Ward 3</h2>
 
-        <li>
-          <h2>Ward 5</h2>
+        <ul class="default">
+          <li><strong><a href="https://drive.google.com/file/d/1-LEMnRxNOvwfTnfPUHJ8ebbd8jVfm8qB/view?usp=sharing">Ben Ewen-Campen <span class="incumbent">*</span></a></strong></li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li><a href="https://drive.google.com/file/d/1-k89jnkD2BZrgpU4q7nY746D488f_8pj/view?usp=sharing">Tessa Bridge</a></li>
-            <li><a href="https://drive.google.com/file/d/1-hpM-dVRccjZyayD42BvMvWBqbQ7MqZj/view?usp=sharing">Todd Easton</a></li>
-            <li><a href="https://drive.google.com/file/d/1-aInfhVwdcptVOeoQJEw1yLsKr7aZG4n/view?usp=sharing">Beatriz Gómez-Mouakad</a></li>
-          </ul>
-        </li>
+      <li>
+        <h2>Ward 4</h2>
 
-        <li>
-          <h2>Ward 6</h2>
+        <ul class="default">
+          <li>Jesse Clingan <span class="incumbent">*</span> (no answers provided)</li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li>Lance Davis <span class="incumbent">*</span> (no answers provided)</li>
-          </ul>
-        </li>
+      <li>
+        <h2>Ward 5</h2>
 
-        <li>
-          <h2>Ward 7</h2>
+        <ul class="default">
+          <li><a href="https://drive.google.com/file/d/1-k89jnkD2BZrgpU4q7nY746D488f_8pj/view?usp=sharing">Tessa Bridge</a></li>
+          <li><a href="https://drive.google.com/file/d/1-hpM-dVRccjZyayD42BvMvWBqbQ7MqZj/view?usp=sharing">Todd Easton</a></li>
+          <li><strong><a href="https://drive.google.com/file/d/1-aInfhVwdcptVOeoQJEw1yLsKr7aZG4n/view?usp=sharing">Beatriz Gómez-Mouakad</a></strong></li>
+        </ul>
+      </li>
 
-          <ul class="default">
-            <li><a href="https://drive.google.com/file/d/1-SrCRrUzvnhGFmVaFfYHjGtGFMUvMWJs/view?usp=sharing">Alex Anderson</a></li>
-            <li><a href="https://drive.google.com/file/d/1-EGBqYKGQTCAB8j6K6ys7X0LgwV5eXJg/view?usp=sharing">Maria Koutsoubaris</a></li>
-            <li><a href="https://drive.google.com/file/d/1-gZW8SgYbfnKQjlNOSjPkKhkh51geAl4/view?usp=sharing">Becca Miller</a></li>
-            <li><a href="https://drive.google.com/file/d/1-ZkO5Bqbl9cj5g6dafnGbc4NIan4CGGw/view?usp=sharing">Judy Pineda Neufeld</a></li>
-          </ul>
-        </li>
-      </ul>
-    </article>
+      <li>
+        <h2>Ward 6</h2>
+
+        <ul class="default">
+          <li>Lance Davis <span class="incumbent">*</span> (no answers provided)</li>
+        </ul>
+      </li>
+
+      <li>
+        <h2>Ward 7</h2>
+
+        <ul class="default">
+          <li><a href="https://drive.google.com/file/d/1-SrCRrUzvnhGFmVaFfYHjGtGFMUvMWJs/view?usp=sharing">Alex Anderson</a></li>
+          <li><a href="https://drive.google.com/file/d/1-EGBqYKGQTCAB8j6K6ys7X0LgwV5eXJg/view?usp=sharing">Maria Koutsoubaris</a></li>
+          <li><a href="https://drive.google.com/file/d/1-gZW8SgYbfnKQjlNOSjPkKhkh51geAl4/view?usp=sharing">Becca Miller</a></li>
+          <li><strong><a href="https://drive.google.com/file/d/1-ZkO5Bqbl9cj5g6dafnGbc4NIan4CGGw/view?usp=sharing">Judy Pineda Neufeld</a></strong></li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <p>Here, we present some additional context on each candidate we endorsed, as well as candidates we consider good choices, even though we did not feel confident endorsing them.</p>
+
+    <h2>Mayor</h2>
+
+    <p>Somerville has three very good candidates for mayor this year, and we would not be disappointed to see Katjana Ballantyne, Mary Cassesso or Will Mbah in the mayor’s office this fall. We did, however, ultimately decide to endorse Katjana & Mary.</p>
+
+    <h3>Endorse</h3>
+
+    <h4>Katjana Ballantyne</h4>
+    <p>Katjana supports our three key goals of housing production, reducing auto-dependence, and enhancing tenant protections. Our conversation with her and her comments on our survey gave us a great deal of confidence about her thoughtful and informed understanding of the intersection of commercial development, tax base, climate change, and funding for affordable housing. She also has the most experience on city council.</p>
+
+    <h4>Mary Cassesso</h4>
+    <p>Like Katjana & Will, Mary also supports our key goals and has a nuanced and thoughtful view about achieving them. She does not have experience on City Council, but we believe that her experience as an executive manager leading a large organization would make her very effective.</p>
+
+    <h3>Also Good</h3>
+
+    <h4>Will Mbah</h4>
+    <p>Will didn’t fill out our survey, but we spoke with him about the issues. He too is aligned with Somerville YIMBY on increasing housing, moving away from auto-centric patterns, and strengthening tenant protections. He’s an important voice in the city and his emphasis on creating community benefits for large projects is excellent. He has spent less time on City Council than Katjana Ballantyne, but his record as a councilor has been excellent. We are, however, slightly concerned about a plan to<span class="c8"><a class="c6" href="https://www.google.com/url?q=https://www.wgbh.org/news/politics/2021/08/04/whats-at-stake-in-somervilles-mayoral-race&amp;sa=D&amp;source=editors&amp;ust=1629477724835000&amp;usg=AOvVaw2kSdTxdD1Bm7mRkScjXakG">increase inclusionary zoning requirements he mentioned in a recent WGBH article</a>. We feel that raising the requirement above 20% risks becoming counterproductive by reducing the total amount of housing produced, harming people who need subsidized and market-rate housing alike.</p>
+
+    <h2>Councilor At-large</h2>
+
+    <p>We have chosen to endorse Willie Burnley, Charlotte Kelly, and Jake Wilson.</p>
+
+    <p>Eve Seitchik, Justin Klekota and Kristin Strezo are also good choices.</p>
+
+    <h3>Endorse</h3>
+
+    <h4>Willie Burnley</h4>
+    <p>Willie is in support of our key goals on increasing housing, moving away from auto-centric development, and enhancing tenant protections. He is more skeptical of market-rate housing than we are, but his approach to all the issues is deeply thought and nuanced. We know that he would be an excellent City Councilor.</p>
+
+    <h4>Charlotte Kelly</h4>
+    <p>Charlotte aligns with us on creation of affordable housing, moving away from auto-centric development, and tenant protections. She is much more skeptical of market-rate housing than we are, but we feel that she’s still a great choice. Her response notes that &ldquo;any serious approach to housing and development here in Somerville must have thoughtful processes rooted in equity, justice, and systemic solutions,&rdquo; and we agree entirely. </p>
+
+    <h4>Jake Wilson</h4>
+    <p>Jake aligns with us on housing production, transit/parking issues, and tenant protections. His written responses reflect a nuanced approach to development and other issues and an acknowledgement of the tradeoffs necessary in governance. For example, he noted that to achieve our goal of increasing the amount of green space in the city, we’ll also need tall buildings to balance it.</p>
+
+    <h3>Also Good</h3>
+
+    <h4>Justin Klekota</h4>
+    <p>Justin agrees with us about housing and tenant protections, but supports building more parking, which is counterproductive for both climate and traffic.</p>
+
+    <h4>Eve Seitchik</h4>
+    <p>Eve did not complete our survey, but we spoke with them briefly about the issues. Critically, they agree with us about the need to create affordable housing, and on climate and transit issues. Although we disagree with them about the relative value of market-rate housing production, Eve is a thoughtful and forceful advocate for climate, transit and housing justice.</p>
+
+    <h4>Kristen Strezo</h4>
+    <p>Kristin’s survey answers state that she aligns with us on housing production, transit/parking issues, and tenant protections. She does not support reallocating police funding to housing and social services. She is also the sole incumbent running for an at-large seat. Her experience could be valuable, but she has not proven to be an especially effective council member thus far.</p>
+
+    <h2>Ward 1</h2>
+
+    <h4>Matt McLaughlin</h4>
+    <p>Matt is running unopposed & did not respond to our survey, so we have not prepared an analysis of his positions.</p>
+
+    <h2>Ward 2</h2>
+
+    <p>We like both of these candidates and think that either would do an excellent job.</p>
+
+    <h4>Steph Aman</h4>
+    <p>Steph supports increased housing and tenant protections, and we also like his emphasis on trees and green space. We disagree with him about parking issues. His track record as a community organizer and activist is excellent and we are confident that he’d do a good job as a councilor as well. He was also a co-chair of the SomerVision 2040 committee, and his experience there would be very valuable on the City Council.</p>
+
+    <h4>JT Scott</h4>
+    <p>JT aligns with us on housing, transit/parking, and tenant protections. He is very skeptical of market-rate housing, but has an excellent track record of building community engagement to reach effective goals. His work guiding the project at 346 Somerville Ave (the former J.J. Sullivan building) to an excellent and unconventional outcome is a great example of his pragmatic approach. He was one of several candidates to push back against the oversimplification in our survey, and made several excellent points in our conversation about it.</p>
+
+    <h2>Ward 3</h2>
+
+    <h3>Endorse</h3>
+
+    <h4>Ben Ewen-Campen</h4>
+    <p>Ben is running unopposed, but answered our survey anyway. He aligns with all three of our core issues, and his written comments reveal a very thoughtful approach to city government.</p>
+
+    <h2>Ward 4</h2>
+
+    <h4>Jesse Clingan</h4>
+    <p>Jesse is running unopposed & did not respond to our survey, so we have not prepared an analysis of his positions.</p>
+
+    <h2>Ward 5</h2>
+
+    <p>We have chosen to endorse Beatriz Gómez Mouakad, although Tessa Bridge would also be a good choice.</p>
+
+    <h3>Endorse</h3>
+
+    <h4>Beatriz Gómez Mouakad</h4>
+    <p>Beatriz aligns with us on increasing overall housing supply, addressing transit/parking issues, and tenant protections. We feel that her experience working on affordable housing for Just-A-Start means that she knows both how important affordable housing is, and how to create it. She told us that she would prioritize affordable housing over market rate, but recognizes the need to make that housing economically viable through mixed-use and mixed-income&nbsp;development.</p>
+
+    <h3>Also Good</h3>
+
+    <h4>Tessa Bridge</h4>
+    <p>Tessa is supportive of affordable housing and tenant protections, but not of market-rate housing. She’s also a great advocate for people with disabilities. In her written comments she noted that while she supports many of our positions in general, it is important to include an equity focus when considering any of them — and we agree with that.</p>
+
+    <h2>Ward 6</h2>
+
+    <h4>Lance Davis</h4>
+    <p>Lance is running unopposed & did not respond to our survey, so we have not prepared an analysis of his positions.</p>
+
+    <h2>Ward 7</h2>
+
+    <p>Three of these candidates seem very good, but we have chosen to endorse Judy&nbsp;Pineda&nbsp;Neufeld.</p>
+
+    <h3>Endorse</h3>
+
+    <h4>Judy Pineda Neufeld</h4>
+    <p>Judy aligns with us on increasing housing supply, addressing transit/parking issues, and tenant protections. She supports added density near GLX stations to create more affordable housing, an expansion of the Affordable Housing Overlay, and reduced mandatory parking requirements.</p>
+
+    <h3>Also Good</h3>
+
+    <h4>Alexander H Anderson</h4>
+    <p>Alexander aligns with us on increasing housing supply, addressing transit/parking issues, and tenant protections. He states that he would focus on and prioritize affordable housing over market-rate, and notes that nuance and systems thinking are important.</p>
+
+    <h4>Becca Miller</h4>
+    <p>Becca aligns with us on housing, transit and tenant protections but is much more skeptical of market-rate housing.</p>
+  </section>
+
+  <hr>
+
+  <p>If you’ve read all the way through, thanks for taking the time! We hope this is a helpful resource, and we welcome any feedback you might have at&nbsp;<a href="mailto:steering@somervilleyyimby.org?subject=2021 Election Guide">steering@somervilleyyimby.org</a>.</p>
+</article>
 <?php require_once "../../includes/footer.php";
